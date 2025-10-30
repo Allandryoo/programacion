7450aadad187a0dcd2a757ded33db9227ecbca87
allandryoo <asantosd@alumnes.santjosepobrer.es>
2025-10-30 20:16:45 +0100

    for anidados

---
+++ UD2/Actividades 12-14/for6.py
@@ -0,0 +1,12 @@
+altura=5
+
+for i in range(1,altura):
+    print("* "*altura)
+    for j in range(1,altura-1):
+        if i == 1 or 1 == altura:
+            print("* "*i+" "*altura + " *")
+    print("* "*altura)
+    break
+
+    
+    
---
+++ UD2/Actividades 12-14/for7.py
@@ -0,0 +1,5 @@
+altura=5
+j=altura
+for i in range(1,altura+1):
+    print(" "*j + "*"*i)
+    j-=1

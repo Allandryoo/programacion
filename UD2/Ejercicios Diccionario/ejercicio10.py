030ef15397d2a8b1ed3068ff99345339bdee6aa2
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2025-11-13 19:53:02 +0100

    dicc 2

--- UD2/Ejercicios Diccionario/ejercicio10.py
+++ UD2/Ejercicios Diccionario/ejercicio10.py
@@ -1,5 +1,7 @@
 alumnos={"nombre": "alan","nota" : 10,"nombre":"ionut","nota":3,"nombre":"boy","nota":6,"nombre":"carlos","nota":4,"nombre":"patricio","nota":7}
 alumnos_aprobados={}
-for value in alumnos.items():
+for key, value in alumnos.items():
     if value["nota"] >= 5:
-        alumnos_aprobados.update()
+        alumnos_aprobados.update({"key":value})
+
+print(alumnos_aprobados)

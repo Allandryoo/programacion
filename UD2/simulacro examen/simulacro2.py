c7dd51395a57e0b919eee33de94f5eb3949dd3f2
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2025-11-20 16:41:27 +0100

    terminasdo

--- UD2/simulacro examen/simulacro2.py
+++ UD2/simulacro examen/simulacro2.py
@@ -23,17 +23,17 @@ utensilios = ["cuchillo.",
               "cuchara.",
               "tenedor."]
 
-menu_opciones = ["Salir",
-                 "Agregar ingrediente",
-                 "Agregar accion",
-                 "Agregar utensilio",
-                 "Mostrar ingredientes",
-                 "Mostrar acciones",
-                 "Mostrar utensilios",
-                 "Crear instruccion",
-                 "Crear receta",
-                 "Crear libro",
-                 "Mostrar recetario"]
+menu_opciones = {"S":"Salir",
+                 1:"Agregar ingrediente",
+                 2:"Agregar accion",
+                 3:"Agregar utensilio",
+                 4:"Mostrar ingredientes",
+                 5:"Mostrar acciones",
+                 6:"Mostrar utensilios",
+                 7:"Crear instruccion",
+                 8:"Crear receta",
+                 9:"Crear libro",
+                 10:"Mostrar recetario"}
 
 
 def numero_random(lista_random):
@@ -88,10 +88,16 @@ def crear_receta(numero_pasos):
 
 
 def crear_libro(nombre_libro, numero_recetas):
-
+    lista_libro=[]
     for i in range(1, numero_recetas+1):
         numero = int(input(f"Indica el numero de pasos para la receta {i}"))
-        recetario[nombre_libro] = {f"receta{i}": crear_receta(numero)}
+        lista_libro.append(f"receta{i} {crear_receta(numero)}")
+    recetario[nombre_libro]=[lista_libro]
+
+def mostrar_recetario():
+
+    for key,value in recetario.items():
+        print(key,value)
 
 
 while not salir:
@@ -99,66 +105,66 @@ while not salir:
     print("\n-----------------------------------------------")
     print("              Menu de opciones")
     print("-----------------------------------------------")
-    for posicion_lista, opcion_lista in enumerate(menu_opciones):
+    for posicion_lista, opcion_lista in menu_opciones.items():
         print(f"Presiona {posicion_lista} para: {opcion_lista}.")
     print("-----------------------------------------------")
-    opcion = int(input("Escoe una opcion:\n"))
+    opcion = str(input("Escoe una opcion:\n")).lower()
 
-    if opcion == 0:
+    if opcion == "s":
 
         print("Hoy no comes")
         salir = True
 
-    elif opcion == 1:
+    elif opcion == "1":
 
         ingrediente_agregado = input(
             "Indique el ingrediente que desear añadir.\n")
         agregar_ingrediente(ingrediente_agregado)
         print(ingredientes)
 
-    elif opcion == 2:
+    elif opcion == "2":
 
         accion_agregada = input(
             "Indique la accion que desear añadir.\n")
         agregar_accion(accion_agregada)
         print(acciones)
 
-    elif opcion == 3:
+    elif opcion == "3":
 
         utensilio_agregado = input(
             "indique el utensilio que desea añadir.\n")
         agregar_utensilio(utensilio_agregado)
         print(utensilios)
 
-    elif opcion == 4:
+    elif opcion == "4":
 
         mostrar_ingredientes()
 
-    elif opcion == 5:
+    elif opcion == "5":
 
         mostrar_acciones()
 
-    elif opcion == 6:
+    elif opcion == "6":
 
         mostrar_utensilios()
 
-    elif opcion == 7:
+    elif opcion == "7":
 
         instruccion_creada = crear_instruccion()
         print(instruccion_creada)
 
-    elif opcion == 8:
+    elif opcion == "8":
 
         numero_pasos = int(input("Indique el numero de pasos:\n"))
         print(crear_receta(numero_pasos))
 
-    elif opcion == 9:
+    elif opcion == "9":
 
         nombre_libro = input("Indique el nombre del libro:\n")
         numero_recetas = int(input("Indique el numero de recetas:\n"))
 
         crear_libro(nombre_libro, numero_recetas)
 
-    elif opcion == 10:
+    elif opcion == "10":
 
-        print(recetario)
+        mostrar_recetario()

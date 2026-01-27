3f8b2188ff727e41bb13efb769934ef5339ae617
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-01-27 17:30:23 +0100

    ase

--- UD4/Pokemon/pokemon.py
+++ UD4/Pokemon/pokemon.py
@@ -6,19 +6,16 @@ with open(r"C:\Users\ALLANDRYOSANTOSDACON\Documents\programacion_sjo\UD4\Pokemon
     for linea in lista_pokemons:
             almacen.append(linea.strip())
 print(almacen)
-for i,j,h,l in almacen:
-    print(i, j, h, l)
 
-
-    planta = 0
-    volador = 0
-    fuego = 0
-    agua = 0
+planta = 0
+volador = 0
+fuego = 0
+agua = 0
         
 class Pokemon:
     def __init__(self, nombre, tipo, ataque, defensa, ps):
         self.nombre = nombre
-        self.tipo = ["planta", "volador", "fuego", "agua"]
+        self.tipo = tipo
         self.ataque = ataque
         self.defensa = defensa
         self.ps = 100

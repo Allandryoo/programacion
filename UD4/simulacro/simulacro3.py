a35d21376b34638085adae128ee2156cab9a71b5
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-02-15 16:07:09 +0100

    dasd

--- UD4/simulacro/simulacro3.py
+++ UD4/simulacro/simulacro3.py
@@ -10,11 +10,12 @@ class Curso:
 
     def mostrar_info(self):
         return f"Codigo:{self.codigo} | Titulo:{self.titulo} |Horas:{self.horas} | Precio:{self.precio}"
-    
+
     def precio_final(self):
         preciofinal = self.horas * self.precio
         return preciofinal
-    
+
+
 class CursoOnline(Curso):
     def __init__(self, codigo, titulo, horas, precio, plataforma):
         super().__init__(codigo, titulo, horas, precio)
@@ -22,11 +23,12 @@ class CursoOnline(Curso):
 
     def mostrar_info(self):
         return f"Codigo:{self.codigo} | Titulo:{self.titulo} | Horas:{self.horas} | Precio:{self.precio} | Plataforma:{self.plataforma}"
-    
+
     def precio_final(self):
         preciofinal = self.horas * self.precio
         return preciofinal
-    
+
+
 class CursoPresencial(Curso):
     def __init__(self, codigo, titulo, horas, precio, aula):
         super().__init__(codigo, titulo, horas, precio)
@@ -34,15 +36,16 @@ class CursoPresencial(Curso):
 
     def mostrar_info(self):
         return f"Codigo:{self.codigo} | Titulo:{self.titulo} | Horas:{self.horas} | Precio:{self.precio} | Aula:{self.aula}"
-    
+
     def precio_final(self):
         if self.horas > 40:
-            preciofinal = self.horas * (self.precio *1.15)
-            return preciofinal 
+            preciofinal = self.horas * (self.precio * 1.15)
+            return preciofinal
         else:
             preciofinal = self.horas * self.precio
             return preciofinal
-        
+
+
 def crear_curso(tipo):
     codigocurso = int(input("Indica el codigo del curso: "))
     titulocurso = input("Indica el Titulo del curso: ")
@@ -51,42 +54,52 @@ def crear_curso(tipo):
 
     match tipo.upper():
         case "O":
-            plataformacurso = input("Indica la plataforma en la que se hara el curso:\n")
+            plataformacurso = input(
+                "Indica la plataforma en la que se hara el curso:\n")
             return CursoOnline(codigocurso, titulocurso, horascurso, preciocurso, plataformacurso)
         case "P":
             aulacurso = input("Indica el aula del curso:\n")
             return CursoPresencial(codigocurso, titulocurso, horascurso, preciocurso, aulacurso)
 
+
 def agregar_curso(tipo, curso_creado):
     match tipo.upper():
         case "O":
-            cursos.update({curso_creado.codigo : {"titulo":curso_creado.titulo,"horas":curso_creado.horas,"precio":curso_creado.precio}})
+            cursos.update({curso_creado.codigo: {"titulo": curso_creado.titulo, "horas": curso_creado.horas,
+                          "precio": curso_creado.precio, "plataforma": curso_creado.plataforma}})
         case "P":
-            cursos.update({curso_creado.codigo : {"titulo":curso_creado.titulo,"horas":curso_creado.horas,"precio":curso_creado.precio}})
+            cursos.update({curso_creado.codigo: {"titulo": curso_creado.titulo,
+                          "horas": curso_creado.horas, "precio": curso_creado.precio, "aula": curso_creado.aula}})
+
 
 def menu():
     for key, value in opciones.items():
         print(f"Presiona {key} para: {value}.")
 
+
 def mostrar_cursos():
-    for key,value in cursos.items():        
-        print(f"Codigo:{key} | Titulo:{value}")
-salir=True
-opciones={
-    "A":"Añadir curso",
-    "M":"Mostrar informacion cursos",
-    "P":"Calcular precio curso",
-    "S":"Salir"
+    for key, value in cursos.items():
+        print(
+            f"Codigo:{key} | {value}")
+
+
+salir = True
+opciones = {
+    "A": "Añadir curso",
+    "M": "Mostrar informacion cursos",
+    "P": "Calcular precio curso",
+    "S": "Salir"
 }
-c1 = CursoPresencial(1,"DAW",51,12,"4A")
-c2 = CursoPresencial(2,"DAM",38,12,"4A")
-c3 = CursoOnline(3,"ASIX", 45, 11, "Netflix")
+c1 = CursoPresencial(1, "DAW", 51, 12, "4A")
+c2 = CursoPresencial(2, "DAM", 38, 12, "4A")
+c3 = CursoOnline(3, "ASIX", 45, 11, "Netflix")
 
-listacursos=[c1,c2,c3]
+listacursos = [c1, c2, c3]
 
-cursos={}
+cursos = {}
 for i in listacursos:
-    cursos.update({i.codigo : {"titulo":i.titulo,"horas":i.horas,"precio":i.precio}})
+    cursos.update(
+        {i.codigo: {"titulo": i.titulo, "horas": i.horas, "precio": i.precio}})
 
 while salir:
 

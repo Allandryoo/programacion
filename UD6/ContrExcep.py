1f35ac963f527469f1becd5a7563e6cc83bd775f
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-02-26 19:50:44 +0100

    das

--- UD6/ContrExcep.py
+++ UD6/ContrExcep.py
@@ -1,3 +1,22 @@
+class ErrorCantidadMinima(Exception):
+    pass
+class ErrorCantidadMaxima(Exception):
+    pass
+class ErrorNegativo(Exception):
+    pass
+
+def validar_negativo(numero):
+    if numero < 0:
+        raise ErrorNegativo("No puedes ingresar un cantidad negativa")
+
+def validar_maximo(numero):
+    if numero < 0:
+        raise ErrorCantidadMaxima("Cantidad maxima a ingresar 50.000€")
+
+def validar_minimo(numero):
+    if numero < 0:
+        raise ErrorCantidadMinima("Cantidad minima a ingresar 5€")
+    
 class cliente:
     def __init__(self, nombre, saldo):
         self.nombre = nombre
@@ -5,19 +24,25 @@ class cliente:
 
     def ingresar_dinero(self, cantidad):
         self.saldo = self.saldo + cantidad
+        print("Saldo actualizado")
 
     def retirar_dinero(self, cantidad):
         self.saldo = self.saldo - cantidad
-
-def ingresar(cliente):
-    print(f"Saldo: {cliente.saldo}")
-    cantidad = int(input("Indica la cantidad a ingresar\n"))
-    cliente.ingresar_dinero(cantidad)
 
 c1 = cliente("A", 1000)
 
 while True:
     try:
-        ingresar(c1)
-    except ValueError:
-        print("Valor erroneo")
+        print(f"Saldo: {c1.saldo}")
+        cantidad = int(input("Indica la cantidad a ingresar\n"))
+        validar_maximo(cantidad)
+        validar_minimo(cantidad)
+        validar_negativo(cantidad)
+        c1.ingresar_dinero(cantidad)
+    except ErrorCantidadMaxima as max:
+        print(max)
+    except ErrorCantidadMinima as min:
+        print(min)
+    except ErrorNegativo as neg:
+        print(neg)
+        
